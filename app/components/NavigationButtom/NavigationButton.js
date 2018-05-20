a2974897094938b65c4fe2444895d903f103a19e
André Affonso <demelo-10@hotmail.com>
2018-05-20 12:32:18 -0300

    [CREATE] call waiter button

--- app/components/NavigationButtom/NavigationButton.js
+++ app/components/NavigationButtom/NavigationButton.js
@@ -4,23 +4,32 @@ import { withStyles } from 'material-ui/styles';
 import BottomNavigation, {
   BottomNavigationAction,
 } from 'material-ui/BottomNavigation';
-
+import Button from 'material-ui/Button';
 // ICONS
+import HumanGreeting from 'mdi-material-ui/HumanGreeting';
 import Silverware from 'mdi-material-ui/Silverware';
 import CurrencyUsd from 'mdi-material-ui/CurrencyUsd';
 
-import { styles } from './styles';
+import { styles, BottomWrapper, ButtonContainer } from './styles';
 
 const SimpleBottomNavigation = ({ context, classes, changeTableContext }) => (
-  <BottomNavigation
-    value={context}
-    onChange={(event, value) => changeTableContext(value)}
-    showLabels
-    className={classes.root}
-  >
-    <BottomNavigationAction label="Cardápio" icon={<Silverware />} />
-    <BottomNavigationAction label="Comanda" icon={<CurrencyUsd />} />
-  </BottomNavigation>);
+  <BottomWrapper>
+    <ButtonContainer>
+      <Button variant="raised" color="primary">
+        <HumanGreeting /> Chamar garçom
+      </Button>{' '}
+    </ButtonContainer>
+    <BottomNavigation
+      value={context}
+      onChange={(event, value) => changeTableContext(value)}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Cardápio" icon={<Silverware />} />
+      <BottomNavigationAction label="Comanda" icon={<CurrencyUsd />} />
+    </BottomNavigation>
+  </BottomWrapper>
+);
 
 SimpleBottomNavigation.propTypes = {
   classes: PropTypes.object.isRequired,
